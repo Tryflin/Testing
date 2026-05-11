b47eb2673885a2af9305dc57eef70ee17ea76742
Tryflin <tristianjurcaldeu@gmail.com>
2026-05-11 18:48:06 -0500

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,23 +1,30 @@
 <!--Author of Task Management System About Page: Ben Phan
     Navbar split into smaller file  -->
+<?php
+require_once 'init.php';
+?>
 
 <link rel="stylesheet" href="global.css">
 
 <nav>
     <span class="logo">Task Management System</span>
+
     <ul>
         <li><a href="calendar.php">Calendar</a></li>
         <li><a href="index.php">About</a></li>
         <li><a href="contact.php">Contact</a></li>
     </ul>
-    <?php
-    session_start();
-     if(isset($_SESSION['username'])) {
-         echo '<a href="calendar.html" class="nav-button">'
-        . htmlspecialchars($_SESSION['username']) .
-        '</a>';
-    } else{
-        echo '<a href="signup.php" class="nav-button">Get Started</a>';
-    }
-    ?>
+
+    <?php if (isset($_SESSION['user_id'])): ?>
+
+        <a href="calendar.php" class="nav-button">
+            User #<?= htmlspecialchars($_SESSION['user_id']) ?>
+        </a>
+
+    <?php else: ?>
+
+        <a href="signup.php" class="nav-button">Get Started</a>
+
+    <?php endif; ?>
+
 </nav>
